f9ca98bd7998fc372672ec27b730c080fe15afc7
TEDWZombies <tedwzombies@gmail.com>
2012-07-15 19:32:10 -0500

    pg

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TEDWZombies::Application.config.secret_token = '11d1b7db509838334db7c55f4d4c509438f44c92547baef48934c641efb744c736eed32ff5af1a0f5a2009e4f749c42f5ff2c8d7741a82a0ecd7a401e7b9be84'
